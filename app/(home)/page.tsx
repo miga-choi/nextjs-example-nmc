44aa1288ab2401a4f7f27284729d12dd9b97eb5b
miga-choi <miga.choi.1004@gmail.com>
2024-03-25 21:55:36 +0900

    nmc: try parallel requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 export const metadata = {
     title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+export const API_URL: string = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(URL);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
@@ -15,7 +17,7 @@ export default async function HomePage() {
     const movies = await getMovies();
     return (
         <div>
-            {JSON.stringify(movies)}
+            {movies.map(movie => <li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
         </div>
     );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,24 @@
-export default function MovieDetail({params: {id}}: { params: { id: string } }) {
-    console.log('id => ', id);
+import {API_URL} from "../../../(home)/page";
+
+async function getMovie(id: string) {
+    console.log(`Fetching movies: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(`${API_URL}/${id}`);
+    return response.json();
+}
+
+async function getVideos(id: string) {
+    console.log(`Fetching videos: ${Date.now()}`);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieDetail({params: {id}}: { params: { id: string } }) {
+    console.log(`Start fetching: ${Date.now()}`);
+    const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+    console.log(`End fetching: ${Date.now()}`);
     return (
-        <h1>Movie {id}</h1>
+        <h1>{movie.title}</h1>
     );
 }
